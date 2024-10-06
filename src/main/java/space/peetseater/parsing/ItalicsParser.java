cc1e5f792b12f0234a6b58cf3190bf0fb45e2778
Ethan Eldridge <ejayeldridge@gmail.com>
2024-10-05 23:01:12 -0400

    Add italics parser + test

---
+++ src/main/java/space/peetseater/parsing/ItalicsParser.java
@@ -0,0 +1,33 @@
+package space.peetseater.parsing;
+
+import space.peetseater.parsing.ast.AbstractMarkdownNode;
+import space.peetseater.parsing.ast.BoldNode;
+import space.peetseater.parsing.ast.ItalicsNode;
+import space.peetseater.parsing.ast.NullNode;
+import space.peetseater.tokenizer.TokenList;
+import space.peetseater.tokenizer.tokens.StarToken;
+import space.peetseater.tokenizer.tokens.TextToken;
+import space.peetseater.tokenizer.tokens.UnderscoreToken;
+
+/**
+ * Parses __TEXT__ as italics Text
+ * Parses _TEXT_ as italics text
+ **/
+public class ItalicsParser extends TokenParser {
+    @Override
+    public AbstractMarkdownNode match(TokenList tokenList) {
+        if (tokenList.isEmpty()) {
+            return NullNode.INSTANCE;
+        }
+
+        if (tokenList.typesAheadAre(UnderscoreToken.TYPE, UnderscoreToken.TYPE, TextToken.TYPE, UnderscoreToken.TYPE, UnderscoreToken.TYPE)) {
+            return new ItalicsNode(tokenList.get(2).getValue(), 5);
+        }
+
+        if (tokenList.typesAheadAre(UnderscoreToken.TYPE, TextToken.TYPE, UnderscoreToken.TYPE)) {
+            return new ItalicsNode(tokenList.get(1).getValue(), 3);
+        }
+
+        return NullNode.INSTANCE;
+    }
+}
---
+++ src/test/java/space/peetseater/parsing/ItalicsParserTest.java
@@ -0,0 +1,61 @@
+package space.peetseater.parsing;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import space.peetseater.parsing.ast.AbstractMarkdownNode;
+import space.peetseater.parsing.ast.ItalicsNode;
+import space.peetseater.parsing.ast.NullNode;
+import space.peetseater.tokenizer.TokenList;
+import space.peetseater.tokenizer.tokens.TextToken;
+import space.peetseater.tokenizer.tokens.UnderscoreToken;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItalicsParserTest {
+
+    ItalicsParser italicsParser;
+
+    @BeforeEach
+    public void setup() {
+        italicsParser = new ItalicsParser();
+    }
+
+
+    @Test
+    public void empty_list_returns_null_node() {
+        TokenList tokens = new TokenList(List.of());
+        AbstractMarkdownNode node = italicsParser.match(tokens);
+        assertEquals(NullNode.INSTANCE, node);
+    }
+
+    @Test
+    public void matches_star_star_text_star_star_as_bold() {
+        TokenList tokens = new TokenList(List.of(
+                UnderscoreToken.INSTANCE,
+                UnderscoreToken.INSTANCE,
+                new TextToken("this text!"),
+                UnderscoreToken.INSTANCE,
+                UnderscoreToken.INSTANCE
+        ));
+        AbstractMarkdownNode node = italicsParser.match(tokens);
+        assertEquals(ItalicsNode.TYPE, node.getType());
+        assertEquals(5, node.getConsumed());
+        assertEquals("this text!", ((ItalicsNode) node).getValue() );
+    }
+
+    @Test
+    public void matches_star_text_star_as_bold() {
+        TokenList tokens = new TokenList(List.of(
+                UnderscoreToken.INSTANCE,
+                new TextToken("this text!"),
+                UnderscoreToken.INSTANCE
+        ));
+        AbstractMarkdownNode node = italicsParser.match(tokens);
+        assertEquals(ItalicsNode.TYPE, node.getType());
+        assertEquals(3, node.getConsumed());
+        assertEquals("this text!", ((ItalicsNode) node).getValue() );
+    }
+
+}
