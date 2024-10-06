b1546987e5a8c940549885917fb7cd367b9848df
Ethan Eldridge <ejayeldridge@gmail.com>
2024-10-05 22:48:00 -0400

    Make UnderscoreToken a singleton

--- src/main/java/space/peetseater/tokenizer/tokens/UnderscoreToken.java
+++ src/main/java/space/peetseater/tokenizer/tokens/UnderscoreToken.java
@@ -3,9 +3,10 @@ package space.peetseater.tokenizer.tokens;
 public class UnderscoreToken extends ConcreteToken{
 
     public static final String TYPE = "UNDERSCORE";
+    public static final UnderscoreToken INSTANCE = new UnderscoreToken();
     private static final String VALUE = "_";
 
-    public UnderscoreToken() {
+    private UnderscoreToken() {
         super(TYPE, VALUE);
     }
 }
