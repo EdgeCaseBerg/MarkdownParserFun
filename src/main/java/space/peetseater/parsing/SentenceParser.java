5a57be55d1f51023328d8c4d2576abd4585305e5
Ethan Eldridge <ejayeldridge@gmail.com>
2024-10-05 23:30:39 -0400

    Add SentenceParser + test
    Combo of bold, italics or text

---
+++ src/main/java/space/peetseater/parsing/SentenceParser.java
@@ -0,0 +1,11 @@
+package space.peetseater.parsing;
+
+import space.peetseater.parsing.ast.AbstractMarkdownNode;
+import space.peetseater.tokenizer.TokenList;
+
+public class SentenceParser extends TokenParser {
+    @Override
+    public AbstractMarkdownNode match(TokenList tokenList) {
+        return matchFirst(tokenList, new ItalicsParser(), new BoldParser(), new TextParser());
+    }
+}
---
+++ src/test/java/space/peetseater/parsing/SentenceParserTest.java
@@ -0,0 +1,63 @@
+package space.peetseater.parsing;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import space.peetseater.parsing.ast.*;
+import space.peetseater.tokenizer.TokenList;
+import space.peetseater.tokenizer.tokens.StarToken;
+import space.peetseater.tokenizer.tokens.TextToken;
+import space.peetseater.tokenizer.tokens.UnderscoreToken;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SentenceParserTest {
+    SentenceParser sentenceParser;
+
+    @BeforeEach
+    public void setup() {
+        sentenceParser = new SentenceParser();
+    }
+
+    @Test
+    public void return_null_node_on_no_tokens() {
+        AbstractMarkdownNode node = sentenceParser.match(new TokenList(List.of()));
+        assertEquals(NullNode.INSTANCE, node);
+    }
+
+    @Test
+    public void should_parse_text_token_as_sentence() {
+        AbstractMarkdownNode node = sentenceParser.match(new TokenList(List.of(new TextToken("hi"))));
+        assertInstanceOf(TextNode.class, node);
+        assertEquals(1, node.getConsumed());
+        assertEquals("hi", ((TextNode) node).getValue());
+    }
+
+    @Test
+    public void should_parse_bold_token_as_sentence() {
+        AbstractMarkdownNode node = sentenceParser.match(new TokenList(
+                List.of(
+                        StarToken.INSTANCE,
+                        new TextToken("Hi"),
+                        StarToken.INSTANCE
+                )
+        ));
+        assertInstanceOf(BoldNode.class, node);
+        assertEquals(3, node.getConsumed());
+        assertEquals("Hi", ((BoldNode) node).getValue());
+    }
+
+    @Test
+    public void should_parse_italics_token_as_sentence() {
+        AbstractMarkdownNode node = sentenceParser.match(new TokenList(List.of(
+                UnderscoreToken.INSTANCE,
+                new TextToken("Hello"),
+                UnderscoreToken.INSTANCE
+            )
+        ));
+        assertInstanceOf(ItalicsNode.class, node);
+        assertEquals("Hello", ((ItalicsNode) node).getValue());
+        assertEquals(3, node.getConsumed());
+    }
+}
