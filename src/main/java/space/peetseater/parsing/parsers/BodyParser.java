75eeb0892ed17cacb4366a09639deffe409ccfd2
Ethan Eldridge <ejayeldridge@gmail.com>
2024-10-13 23:22:29 -0400

    Make body parser more extensible + parse lists
    Parse lists AND newlines technically, but we did consume the newlines at
     the beginning before, but now we'd consume them whereever they may be.

--- src/main/java/space/peetseater/parsing/parsers/BodyParser.java
+++ src/main/java/space/peetseater/parsing/parsers/BodyParser.java
@@ -5,18 +5,41 @@ import space.peetseater.parsing.ast.BodyNode;
 import space.peetseater.parsing.ast.NullNode;
 import space.peetseater.tokenizer.TokenList;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BodyParser extends TokenParser {
+    List<TokenParser> parsersToApply;
+
+    public BodyParser() {
+         parsersToApply = new LinkedList<TokenParser>();
+         parsersToApply.add(new ParagraphParser());
+         parsersToApply.add(new ListParser());
+         parsersToApply.add(new NewLineParser());
+    }
+
     @Override
     public AbstractMarkdownNode match(TokenList tokenList) {
-        // Consume initial whitesppace
+        // Consume initial whitespace
         MatchedAndConsumed m = matchZeroOrMore(tokenList, new NewLineParser());
-        TokenList prefixTrim = tokenList.offset(m.getConsumed());
+        TokenList trimmed = tokenList.offset(m.getConsumed());
 
-        MatchedAndConsumed paragraphs = matchZeroOrMore(prefixTrim, new ParagraphParser());
-        if (paragraphs.getMatched().isEmpty()) {
-            return NullNode.INSTANCE;
+        List<AbstractMarkdownNode> bodyParts = new LinkedList<>();
+        boolean consumeMore = true;
+        while (consumeMore) {
+            // Match first
+            AbstractMarkdownNode node = matchFirst(trimmed, parsersToApply.toArray(TokenParser[]::new));
+            if (node.isPresent()) {
+                bodyParts.add(node);
+                trimmed = trimmed.offset(node.getConsumed());
+            }
+            consumeMore = node.isPresent();
         }
 
-        return new BodyNode(paragraphs.getMatched(), paragraphs.getConsumed() + m.getConsumed());
+        if (bodyParts.isEmpty()) {
+            return NullNode.INSTANCE;
+        }
+        int bodyPartConsumption = bodyParts.stream().mapToInt(AbstractMarkdownNode::getConsumed).sum();
+        return new BodyNode(bodyParts, bodyPartConsumption + m.getConsumed());
     }
 }
