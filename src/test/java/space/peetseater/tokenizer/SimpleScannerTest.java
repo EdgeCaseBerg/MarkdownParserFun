491efd8faa955bc5f4af5c2f73984cf9f1fdc887
Ethan Eldridge <ejayeldridge@gmail.com>
2024-10-13 21:34:11 -0400

    Make it easier to write tests for tokens

--- src/test/java/space/peetseater/tokenizer/SimpleScannerTest.java
+++ src/test/java/space/peetseater/tokenizer/SimpleScannerTest.java
@@ -1,7 +1,12 @@
 package space.peetseater.tokenizer;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import space.peetseater.tokenizer.tokens.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,64 +26,32 @@ class SimpleScannerTest {
         assertEquals(NullToken.INSTANCE, token);
     }
 
-
-    @Test
-    public void asterisks_tokenize_to_star_type_token() {
+    @ParameterizedTest
+    @MethodSource("provideScannenTokenTestCases")
+    void test_token_literal_becomes_token_type(String input, ConcreteToken expectedToken) {
         SimpleScanner scanner = new SimpleScanner();
-        AbstractToken token = scanner.fromString("*bold*");
-        assertEquals(StarToken.INSTANCE, token);
+        AbstractToken token = scanner.fromString(input);
+        assertEquals(expectedToken, token);
     }
 
-    @Test
-    public void newline_tokenize_to_newline_type_token() {
-        SimpleScanner scanner = new SimpleScanner();
-        AbstractToken token = scanner.fromString("\nnewline");
-        assertEquals(NewLineToken.INSTANCE, token);
+    public static Stream<Arguments> provideScannenTokenTestCases() {
+        return Stream.of(
+                Arguments.of("<", AngleStartToken.INSTANCE),
+                Arguments.of(">", AngleStopToken.INSTANCE),
+                Arguments.of("`", BacktickToken.INSTANCE),
+                Arguments.of("[", BracketStartToken.INSTANCE),
+                Arguments.of("]", BracketEndToken.INSTANCE),
+                Arguments.of("-", DashToken.INSTANCE),
+                Arguments.of("=", EqualsToken.INSTANCE),
+                Arguments.of("\\", EscapeCharacterToken.INSTANCE),
+                Arguments.of("\n", NewLineToken.INSTANCE),
+                Arguments.of("(", ParenStartToken.INSTANCE),
+                Arguments.of(")", ParenStopToken.INSTANCE),
+                Arguments.of("#", PoundToken.INSTANCE),
+                Arguments.of("*", StarToken.INSTANCE),
+                Arguments.of("_", UnderscoreToken.INSTANCE),
+                Arguments.of(":", ColonToken.INSTANCE)
+        );
     }
-
-    @Test
-    public void underscore_tokenize_to_underscore_type_token() {
-        SimpleScanner scanner = new SimpleScanner();
-        AbstractToken token = scanner.fromString("_italics_");
-        assertEquals(UnderscoreToken.INSTANCE, token);
-    }
-
-    @Test
-    public void dash_tokenizes_to_dash_type_token() {
-        SimpleScanner scanner = new SimpleScanner();
-        AbstractToken token = scanner.fromString("- blabla");
-        assertEquals(DashToken.INSTANCE, token);
-    }
-
-    @Test
-    public void bracket_start_tokenizes_to_bracket_start_type_token() {
-        SimpleScanner scanner = new SimpleScanner();
-        AbstractToken token = scanner.fromString("[foo]");
-        assertEquals(BracketStartToken.INSTANCE, token);
-    }
-
-    @Test
-    public void bracket_end_tokenizes_to_bracket_end_type_token() {
-        SimpleScanner scanner = new SimpleScanner();
-        AbstractToken token = scanner.fromString("]:https://blablabla");
-        assertEquals(BracketEndToken.INSTANCE, token);
-    }
-
-    @Test
-    public void paren_start_tokenizes_to_paren_start_type_token() {
-        SimpleScanner scanner = new SimpleScanner();
-        AbstractToken token = scanner.fromString("(https://blabla)");
-        assertEquals(ParenStartToken.INSTANCE, token);
-    }
-
-    @Test
-    public void paren_end_tokenizes_to_paren_end_type_token() {
-        SimpleScanner scanner = new SimpleScanner();
-        AbstractToken token = scanner.fromString(")\n some text");
-        assertEquals(ParenStopToken.INSTANCE, token);
-    }
-
-
-
 
 }
