c83d7f3efad6211b7f53d4de07b8dd0832e88a27
Ethan Eldridge <ejayeldridge@gmail.com>
2024-10-14 18:09:18 -0400

    Add TokenList#nextNonWhitespaceOffset
    Since there will be at least a few places where we need to compute the
    amount of tokens in front of another that are just plain whitespace or
    newlines, it makes sense to be able to easily get past those to check
    for the next token via the offset.

--- src/main/java/space/peetseater/tokenizer/TokenList.java
+++ src/main/java/space/peetseater/tokenizer/TokenList.java
@@ -1,7 +1,10 @@
 package space.peetseater.tokenizer;
 
 import space.peetseater.tokenizer.tokens.AbstractToken;
+import space.peetseater.tokenizer.tokens.NewLineToken;
+import space.peetseater.tokenizer.tokens.TextToken;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
 
@@ -19,6 +22,30 @@ public class TokenList extends Vector<AbstractToken> {
         }
 
         return new TokenList(this.subList(index, this.size()));
+    }
+
+    public int nextNonWhitespaceOffset() {
+        if (this.isEmpty()) {
+            return 0;
+        }
+        boolean isWhitespace = true;
+        Iterator<AbstractToken> iter = iterator();
+        int offset = 0;
+        while (isWhitespace && iter.hasNext()) {
+            AbstractToken next = iter.next();
+            offset++;
+            switch (next.getType()) {
+                case NewLineToken.TYPE ->
+                    isWhitespace = true;
+                case TextToken.TYPE ->
+                    isWhitespace = ((TextToken)next).isEmpty();
+                default -> {
+                    isWhitespace = false;
+                    offset--;
+                }
+            }
+        }
+        return offset;
     }
 
     public boolean typesAheadAre(String ...requiredTypesInARow) {
