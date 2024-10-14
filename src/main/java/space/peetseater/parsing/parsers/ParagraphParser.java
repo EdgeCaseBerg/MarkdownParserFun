6ad874fbe29f3747f7cad89e35ef3c3d3122bdc5
Ethan Eldridge <ejayeldridge@gmail.com>
2024-10-14 17:10:38 -0400

    Add SentencesInterleavedWithNewLines parser
    Allows us to handle the soft newline within a paragraph.

--- src/main/java/space/peetseater/parsing/parsers/ParagraphParser.java
+++ src/main/java/space/peetseater/parsing/parsers/ParagraphParser.java
@@ -7,14 +7,48 @@ import space.peetseater.tokenizer.TokenList;
 import space.peetseater.tokenizer.tokens.EndOfFileToken;
 import space.peetseater.tokenizer.tokens.NewLineToken;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class ParagraphParser extends TokenParser {
     @Override
     public AbstractMarkdownNode match(TokenList tokenList) {
         return matchFirst(
                 tokenList,
                 new SentencesFollowedByTwoNewLines(),
-                new SentencesThenEndOfFileAfterNewLine()
+                new SentencesThenEndOfFileAfterNewLine(),
+                new SentencesInterleavedWithNewLines()
         );
+    }
+
+    static class SentencesInterleavedWithNewLines extends TokenParser {
+        @Override
+        public AbstractMarkdownNode match(TokenList tokenList) {
+            List<AbstractMarkdownNode> matched = new LinkedList<>();
+            int consumed = 0;
+            boolean consumeMore = true;
+            SentenceParser sentenceParser = new SentenceParser();
+            NewLineParser newLineParser = new NewLineParser();
+            MatchedAndConsumed matchedAndConsumed;
+            TokenList tokens = tokenList;
+            do {
+                matchedAndConsumed = matchAtLeastOne(tokens, sentenceParser);
+                boolean hasSentence = !matchedAndConsumed.getMatched().isEmpty();
+                AbstractMarkdownNode newLineMatched = newLineParser.match(tokens.offset(matchedAndConsumed.getConsumed()));
+                boolean followedByNewline = newLineMatched.isPresent();
+                consumeMore = hasSentence && followedByNewline;
+                if (consumeMore) {
+                    matched.addAll(matchedAndConsumed.getMatched());
+                    int localOffset = matchedAndConsumed.getConsumed() + newLineMatched.getConsumed();
+                    consumed += localOffset;
+                    tokens = tokens.offset(localOffset);
+                }
+            } while (consumeMore);
+            if (consumed > 0) {
+                return new ParagraphNode(matched, consumed);
+            }
+            return NullNode.INSTANCE;
+        }
     }
 
     static class SentencesThenEndOfFileAfterNewLine extends TokenParser {
